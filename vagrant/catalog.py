2798957a76d1840a52557635d5bb0b337166a88a
Charles Brands <charles.brands@gmail.com>
2017-08-25 10:08:47 +0200

    routing added

--- vagrant/catalog.py
+++ vagrant/catalog.py
@@ -4,9 +4,33 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello World"
+@app.route('/catalog')
+def showAll():
+    return "mainpage"
+
+@app.route('/login')
+def login():
+    return "loginpage"
+
+@app.route('/catalog/<int:catagory_id>/')
+def showCategory(catagory_id):
+    return "categorypage"
+
+@app.route('/catalog/<int:catagory_id>/new/')
+def newMenuItem(catagory_id):
+    return "page to create a new pet"
+
+
+@app.route('/catalog/<int:catagory_id>/<int:pet_id>/edit/')
+def editMenuItem(catagory_id, pet_id):
+    return "page to edit a pet"
+
+
+
+@app.route('/catalog/<int:catagory_id>/<int:pet_id>/delete/')
+def deleteMenuItem(catagory_id, pet_id):
+    return "page to delete a pet"
+
 
 if __name__ == '__main__':
     app.debug = True
